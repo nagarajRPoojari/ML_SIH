fc6c93479507479514fa37c208e24ed950802219
nagarajRPoojari <np137270@gmail.com>
2023-08-29 23:34:31 +0530

    small bugs

--- src/pipeline/prediction/prediction.py
+++ src/pipeline/prediction/prediction.py
@@ -2,16 +2,16 @@ import os
 import numpy as np
 from scipy.spatial.distance import euclidean
 from pipeline.preprocessing.data_preprocessing import DataPreprocesser
-
+from pipeline.component import Data , DataItem
 class Prediction():
-    def __init__(self):
+    def __init__(self)->None:
         
         self.classes={
             "Severity" : ["high", "low", "medium"],
             "Capacity" : ["large", "medium", "small"],
             "Services" : ["counseling", "food and water", "language assistance", "medical care", "transportation"],
             "Availability" : [ "almost full","available", "full", "temporarily unavailable"],
-            "Specialization" : [  "fire response","medical", "mental health support","search and rescue","water rescue"],
+            "Specialization" : ["fire response","medical", "mental health support","search and rescue","water rescue"],
             "Medical_Facility" : ["basic first aid", "intensive care units", "pediatric care","surgical facilities","trauma care" ],
             "Supply_and_Resource" : [ "limited supplies", "medical equipment available", "pharmaceuticals available","well-stocked"],
             "calamities" : ["earthquake", "fire", "flood",  "hurricane",  "pandemic","tsunami"]
@@ -45,16 +45,26 @@ class Prediction():
         user_data=self.preprocesser.preprocess(user)
         scores=[]
         for rescue in data:
-            rescue_data=self.preprocesser.preprocess(rescue)
-            score=self.similarity(user_data,rescue_data)
-            scores.append((rescue['id'],score))
+            if self.is_available(rescue):
+                rescue_team=self.preprocesser.preprocess(rescue)
+                score=self.similarity(user_data,rescue_team)
+                scores.append((rescue['id'],score))
             
         scores=np.array(scores)
+        print(scores)
+        
         scores_idx=np.argsort(scores[:,-1])
         
         scores=scores[scores_idx[:k]]
-        
         return scores
+    
+    def is_available(self,rescue_team):
+        if rescue_team['Availability'][0]=='available':
+            return True
+        else : 
+            return False 
+
+        
         
         
 
